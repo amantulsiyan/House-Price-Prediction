30eac5f3dad9a553c85182fdd58a9f8eb8b04947
Aman Tulsiyan <amantulsiyan12@gmail.com>
2026-01-19 15:20:11 +0530

    Added Docstrings into the code for better readability.

--- src/data.py
+++ src/data.py
@@ -2,9 +2,17 @@
 #Imports
 
 from sklearn.datasets import fetch_california_housing
-import pandas as pd
-
-data = fetch_california_housing(as_frame=True)
-df = data.frame
-X=df.drop('MedHouseVal',axis=1)
-y=df['MedHouseVal']
+def load_raw_data():
+    '''
+    Fetches California Housing Dataset from sklearn and returns a dataframe
+    '''
+    data = fetch_california_housing(as_frame=True)
+    df = data.frame
+    return df
+def split_features(df):
+    '''
+    Parameters: Takes dataframe 'df' as input and returns the features into X & y variables
+    '''
+    X=df.drop('MedHouseVal',axis=1)
+    y=df['MedHouseVal']
+    return (X,y)
